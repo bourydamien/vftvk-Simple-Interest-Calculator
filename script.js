bcc4326aad013e0c8f7b995bd2f4fd9b4b879bf7
bourydamien <81621656+bourydamien@users.noreply.github.com>
2021-07-31 01:19:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function updateRate()
 function compute()
 {
    
-    document.getElementById("result").innerText=years;
+    document.getElementById("interest").innerText=interest;
     
 }
         
