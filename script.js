bb63c3b7ecafc85e2079e990d3f73cc404ac13a6
bourydamien <81621656+bourydamien@users.noreply.github.com>
2021-07-31 01:51:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
+
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
@@ -11,6 +7,11 @@ function updateRate()
 
 function compute()
 {
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
    
     document.getElementById("interest").innerText=interest;
     
